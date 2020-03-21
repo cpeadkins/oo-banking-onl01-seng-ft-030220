5ba8323724b6c721af2ac4ccc707beb67ba8f306
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 22:37:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,4 +8,30 @@ class Transfer
     @amount = 50
   end
   
+  def valid?
+    @sender.valid? && @receiver.valid?
+    # @status = true
+    true
+  end
+
+  def execute_transaction
+    if @sender.valid? && @receiver.valid?
+      @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @receiver.balance > @amount && @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+  
 end
