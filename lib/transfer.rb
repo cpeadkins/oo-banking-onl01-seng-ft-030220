8c47fbe14fe5184226265cfa602a400b64c12cbd
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 22:36:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status, :transfer_amount
+  attr_accessor :sender, :receiver, :status, amount
   
-  def initialize(sender,receiver,transfer_amount)
+  def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
-    @transfer_amount = 50
+    @amount = 50
   end
   
 end
