973f24a20f14d43bd1cffb81b6941de4cd3cec38
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 22:18:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
-  attr_accessor :account
+  attr_accessor :account, :account_name
   
   def initialize(account)
     @account = account
+    @account_name = account_name
   end
 
 end
