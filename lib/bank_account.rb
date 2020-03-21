d57b40cf6d1abbe392a3c67ae375b74ef351a0b2
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 22:22:16 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
-  attr_accessor :account, :name, :balance
+  attr_accessor :account, :name, :balance, :status
   
   def initialize(name)
     @account = account
     @name = name
     @balance = 1000
+    @status = "open"
   end
 
 end
